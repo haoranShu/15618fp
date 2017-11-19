99eb0ed4380a76dbbdfd7e839cd9c2310c00247f
Jay Yu <jay.yzhe@gmail.com>
2017-11-18 22:57:36 -0500

    combine loops

--- heatmap/heatmap_block.c
+++ heatmap/heatmap_block.c
@@ -100,25 +100,19 @@ void heatmap_add_points_omp_with_stamp(heatmap_t* h, unsigned* xs, unsigned* ys,
         }
     }
 
-    unsigned x, y, k;
-    #pragma omp parallel for
-    for (y = 0; y < h->h; y++)
-    {
-        for (k = 1; k < NUM_OF_BLOCKS; k++)
-        {
-            for (x = 0; x < h->w; x++)
-            {
-                local_heatmap[0].buf[y * h->w + x] += local_heatmap[k].buf[y * h->w + x];
-            }
-        }
-    }
-
+    unsigned x, y, k, i;
     float w;
-    for (y = 0; y < h->h; y++)
+
+    for (y = 0, i = 0; y < h->h; y++)
     {
-        for (x = 0; x < h->w; x++)
+        for (x = 0; x < h->w; x++, i++)
         {
-            w = local_heatmap[0].buf[y * h->w + x];
+            w = local_heatmap[0].buf[i];
+            for (k = 1; k < NUM_OF_BLOCKS; k++)
+            {
+                w += local_heatmap[k].buf[i];
+            }
+
             if (w > 0)
             {
                 heatmap_add_weighted_point_with_stamp(h, x, y, w, stamp);
