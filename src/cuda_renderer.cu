d07a8236c4b9df4a044f1c7ea94c408397f1fff3
Haoran Shu <hshu1@andrew.cmu.edu>
2017-12-10 01:03:04 -0500

    re-organized

--- src/cuda_renderer.cu
+++ src/cuda_renderer.cu
@@ -123,7 +123,6 @@ __global__ void tempMax(float* src, float* dst, int n)
 
 void renderNewPointsCUDA(float x0, float y0, float w, float h, std::string filename)
 {
-    printf("here\n");
     start_cuda = std::clock();
     float pt_width = w * 9 / renderW;
     float pt_height = h * 9 / renderH;
@@ -134,9 +133,9 @@ void renderNewPointsCUDA(float x0, float y0, float w, float h, std::string filen
     float max_weight;
     tempMax<<<1, 1>>>(pixel_weights, max_buf, renderH * renderW);
     cudaMemcpy((void *)&max_weight, (void *)max_buf, 1 * sizeof(float), cudaMemcpyDeviceToHost);
-    printf("here\n");
+    cudaDeviceSynchronize();
+    start_cuda = std::clock();
     writeToImageKernel<<<128, 128>>>(pixel_weights, pixel_color, renderH * renderW, max_weight, heatmap_cs_default);
-    printf("here\n");
     cudaDeviceSynchronize();
     std::cout << (std::clock() - start_cuda) * 1000  / (double) CLOCKS_PER_SEC << " ms\n";
     cudaMemcpy((void *)ppmOutput->data, (void *)pixel_color,
